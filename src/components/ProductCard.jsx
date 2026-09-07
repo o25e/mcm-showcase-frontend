22ba185f48ea62111cc6d23e00778effe7e3c9fc
parkseoyeon9163@gmail.com <162225988+o25e@users.noreply.github.com>
2026-09-07 17:43:45 +0900

    chore: 미사용 상품 상세 페이지 관련 코드 제거

--- src/components/ProductCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProductCard({ name, price, image }) {
-  return (
-    <article className="product-card">
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>{price}</p>
-    </article>
-  );
-}
